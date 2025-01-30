a8db6fefee46dd83ed998c6c74e6ba0109111d81
wanderok <wanderok@msn.com>
2025-01-30 18:51:48 -0300

    roberto

--- wander/code/kill_gazebo_ros.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo killall rosmaster
-sudo killall gzserver
-sudo killall gzclient
--- wander/code/scripts/start_simulation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting simulation: Gazebo without GUI, ROS, SITL, world Grass"
-
-# Starts QGroundControl. This command depends on the location of its .AppImage
-~/Desktop/QGroundControl.AppImage &
-
-roslaunch plt_gazebo simulation.launch gui:=false x:=-5 y:=0 world:=grass
